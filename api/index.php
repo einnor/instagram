7bd1b0ac5dd3d8c86b9c73835af4749c6200fd14
Ronnie Nyaga <rnafundi@gmail.com>
2015-11-26 14:41:52 +0300

    Login Completed

--- api/index.php
+++ api/index.php
@@ -6,10 +6,64 @@ $marray=array();
 global $app;
 
 $app= new Slim();
+$app->get('/user/logout','logout');
+$app->post('/user/login','login');
 $app->get('/users','getAllUsers');
 $app->post('/users','createUser');
 
 $app->run();
+
+function login(){$pdo = getConnection();
+	global $app;
+	$request = $app->request();
+	$reqdata = json_decode($request->getBody());
+	
+	$ss="SELECT u.id,u.username,a.password FROM user u LEFT JOIN account a ON u.id=a.user_id WHERE u.email=? OR u.username=?";
+	$stmt = $pdo->prepare($ss);$stmt->execute(array($reqdata->username,$reqdata->username));
+	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+	if ($stmt->rowCount()>0){
+		include_once '../classes/PasswordHash.php';
+		$t_hasher = new PasswordHash(8, FALSE);	
+		$check = $t_hasher->CheckPassword($reqdata->password, $row['password']);
+		if ($check!=1){
+			$marray['response']="fail";$marray['message']="passwords do not match";
+			echo json_encode($marray);
+			return false;
+		}else{
+			$marray['response']		="success";
+			$marray['username']		=$row['username'];
+			$marray['userid']		=$row['id'];
+			
+			$_SESSION['userid'] = NULL;
+			$_SESSION['username'] = NULL;
+			
+			unset($_SESSION['userid']);
+			unset($_SESSION['username']);
+			
+			$_SESSION['userid']		=$row['id'];
+			$_SESSION['username']	=$row['username'];
+
+			echo json_encode($marray);
+			return false;
+		}
+	}else{
+		$marray['response']="fail";$marray['message']="account does not exist";
+		echo json_encode($marray);
+		return false;	
+	}
+	return true;
+}; //Login
+function logout(){$pdo = getConnection();
+	global $app;
+	$request = $app->request();
+	$reqdata = json_decode($request->getBody());
+	$_SESSION['userid'] = NULL;
+	$_SESSION['username'] = NULL;
+	
+	unset($_SESSION['userid']);
+	unset($_SESSION['username']);
+}
+
 
 function getAllUsers(){
 	$pdo = getConnection();
